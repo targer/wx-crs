6d33165d4ae0a6171d2dfbc62b309dc4c6b264a2
xj <william.xuj@live.com>
2015-12-15 18:05:02 +0800

    add exception handle

---
+++ CRS-cmd-tij/src/main/java/org/open/tij/concurrency/Joining.java
@@ -0,0 +1,59 @@
+package org.open.tij.concurrency;//: concurrency/Joining.java
+// Understanding join().
+
+class Sleeper extends Thread {
+    private int duration;
+
+    public Sleeper(String name, int sleepTime) {
+        super(name);
+        duration = sleepTime;
+        start();
+    }
+
+    public void run() {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            System.out.print(getName() + " was interrupted. " +
+                    "isInterrupted(): " + isInterrupted());
+            return;
+        }
+        System.out.print(getName() + " has awakened");
+    }
+}
+
+class Joiner extends Thread {
+    private Sleeper sleeper;
+
+    public Joiner(String name, Sleeper sleeper) {
+        super(name);
+        this.sleeper = sleeper;
+        start();
+    }
+
+    public void run() {
+        try {
+            sleeper.join();
+        } catch (InterruptedException e) {
+            System.out.print("Interrupted");
+        }
+        System.out.print(getName() + " join completed");
+    }
+}
+
+public class Joining {
+    public static void main(String[] args) {
+        Sleeper
+                sleepy = new Sleeper("Sleepy", 1500),
+                grumpy = new Sleeper("Grumpy", 1500);
+        Joiner
+                dopey = new Joiner("Dopey", sleepy),
+                doc = new Joiner("Doc", grumpy);
+        grumpy.interrupt();
+    }
+} /* Output:
+Grumpy was interrupted. isInterrupted(): false
+Doc join completed
+Sleepy has awakened
+Dopey join completed
+*///:~
