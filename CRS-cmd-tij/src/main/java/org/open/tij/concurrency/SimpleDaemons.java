0a3edfb49727b0fbea90d248964e31b768a49ff2
xj <william.xuj@live.com>
2015-12-18 09:41:06 +0800

    add lock

--- CRS-cmd-tij/src/main/java/org/open/tij/concurrency/SimpleDaemons.java
+++ CRS-cmd-tij/src/main/java/org/open/tij/concurrency/SimpleDaemons.java
@@ -1,28 +1,30 @@
 package org.open.tij.concurrency;//: concurrency/SimpleDaemons.java
 // Daemon threads don't prevent the program from ending.
+
 import java.util.concurrent.*;
 
 
 public class SimpleDaemons implements Runnable {
-  public void run() {
-    try {
-      while(true) {
-        TimeUnit.MILLISECONDS.sleep(100);
-        System.out.print(Thread.currentThread() + " " + this);
-      }
-    } catch(InterruptedException e) {
-      System.out.print("sleep() interrupted");
+    public void run() {
+        try {
+            while (true) {
+                TimeUnit.MILLISECONDS.sleep(100);
+                System.out.print(Thread.currentThread() + " " + this);
+            }
+        } catch (InterruptedException e) {
+            System.out.print("sleep() interrupted");
+        }
     }
-  }
-  public static void main(String[] args) throws Exception {
-    for(int i = 0; i < 10; i++) {
-      Thread daemon = new Thread(new SimpleDaemons());
-      daemon.setDaemon(true); // Must call before start()
-      daemon.start();
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            Thread daemon = new Thread(new SimpleDaemons());
+            daemon.setDaemon(true); // Must call before start()
+            daemon.start();
+        }
+        System.out.print("All daemons started");
+        TimeUnit.MILLISECONDS.sleep(175);
     }
-    System.out.print("All daemons started");
-    TimeUnit.MILLISECONDS.sleep(175);
-  }
 } /* Output: (Sample)
 All daemons started
 Thread[Thread-0,5,main] SimpleDaemons@530daa
